581bd5678046fc36082f01d38f3c0c2b17a02d9a
hafidz1997 <hafidzuddinkf@yahoo.com>
2017-12-06 16:46:52 +0700

    pasien dll

---
+++ page klo mau tambah/loginprocess.php
@@ -0,0 +1,41 @@
+
+<?php
+session_start(); // Starting Session
+$error=''; // Variable To Store Error Message
+if (isset($_POST['submit'])) {
+if (empty($_POST['user']) || empty($_POST['password'])) {?>
+<script language="javascript">alert("Failed to login"); document.location="login.php";</script>;
+<?php
+}
+else
+{
+// Define $username and $password
+	$user=$_POST['user'];
+	$password=$_POST['password'];
+// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+	$connection = mysqli_connect("localhost", "root", "");
+// To protect MySQL injection for Security purpose
+	$user = stripslashes($user);
+	$password = stripslashes($password);
+	$user = mysqli_real_escape_string($connection,$user);
+	$password = mysqli_real_escape_string($connection,$password);
+// Selecting Database
+	$db = mysqli_select_db($connection, "siklikzamzam");
+// SQL query to fetch information of registerd users and finds user match.
+	$query = mysqli_query($connection, "select * from user where password='$password' AND username='$user'");
+	$rows = mysqli_num_rows($query);
+if ($rows == 1) {
+	$_SESSION['login_user']=$user; // Initializing Session
+?>
+	<script language="javascript">alert("Success!"); document.location="index.php";</script>;
+<?php
+	header("location: index.php"); //Redirecting To Other Page
+}else{
+?>
+	<script language="javascript">alert("Failed to login"); document.location="login.php";</script>;
+<?php
+}
+mysqli_close($connection); // Closing Connection
+}
+}
+?>
